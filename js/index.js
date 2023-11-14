4f9393df4751cc1dfcc9177e69923006d035e34e
Zhana Kusman <Zhana.Kusman@gmail.com>
2023-11-13 20:09:09 -0800

    Lesson -4-2

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-const today = new Date();
-const thisYear = today.getFullYear();
-const footer = document.querySelector('footer');
-const copyright = document.createElement('p');
-copyright.innerHTML = "Your Name " + thisYear;
-footer.appendChild(copyright);
+document.addEventListener('DOMContentLoaded', function () {
 
-const skills = ["Skill 1", "Skill 2", "Skill 3", "Skill 4"];
-const skillsSection = document.getElementById('skills');
-const skillsList = skillsSection.querySelector('ul');
+    // Copyright information
+    const today = new Date();
+    const thisYear = today.getFullYear();
+    const footer = document.querySelector('footer');
+    const copyright = document.createElement('p');
+    copyright.innerHTML = "Zhana Kusman " + thisYear;
+    footer.appendChild(copyright);
 
-for (let i = 0; i < skills.length; i++) {
-    const skill = document.createElement('li');
-    skill.textContent = skills[i]; // Set the text content of the skill element
-    skillsList.appendChild(skill);
-}
+    // Skills list
+    const skills = ["Skill 1", "Skill 2", "Skill 3", "Skill 4"];
+    const skillsSection = document.getElementById('skills');
+    const skillsList = skillsSection.querySelector('ul');
+
+    for (let i = 0; i < skills.length; i++) {
+        const skill = document.createElement('li');
+        skill.textContent = skills[i];
+        skillsList.appendChild(skill);
+    }
+});
