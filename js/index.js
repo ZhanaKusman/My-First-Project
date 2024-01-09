648a1e0fe0ad30d1a17af01080f3af38605e228c
Zhana Kusman <Zhana.Kusman@gmail.com>
2024-01-09 15:11:42 -0800

    AJAX Basics

--- js/index.js
+++ js/index.js
@@ -40,11 +40,40 @@ document.addEventListener('DOMContentLoaded', function () {
     removeButton.addEventListener('click', function() {
       const entry = removeButton.parentNode; 
       entry.remove(); 
-      newMessage.appendChild(removeButton); 
+      // newMessage.appendChild(removeButton); 
     });
 
     newMessage.appendChild(messageContent);
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
   });
+
+const githubRequest = new XMLHttpRequest();
+githubRequest.open("GET", "https://api.github.com/users/ZhanaKusman/repos");
+
+githubRequest.addEventListener('load', function () {
+    if (githubRequest.status === 200) {
+        const repositories = JSON.parse(githubRequest.response);
+        console.log(repositories);
+     
+        const projectSection = document.getElementById('project');
+        const projectList = projectSection.querySelector('ul');
+
+    
+        for (let i = 0; i < repositories.length; i++) {
+            const project = document.createElement('li');
+            project.innerText = repositories[i].name;
+
+        
+            projectList.appendChild(project);
+        }
+
+
+
+    } else {
+        console.error('Error fetching GitHub repositories:', githubRequest.statusText);
+    }
+});
+
+githubRequest.send();
 });
